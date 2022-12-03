9a4e2de8ff152eecd0c286eca18637feb8b03209
Roy <royvou@hotmail.com>
2022-12-03 02:09:37 +0100

    feat: Day 2 - Cleanup

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -9,36 +9,13 @@ public class Day02 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    private readonly HashSet<Game> Player1Wins = new HashSet<Game>()
-    {
-        new(Action.Rock, Action.Scissor),
-        new(Action.Scissor, Action.Paper),
-        new(Action.Paper, Action.Rock),
-    };
-
     public override ValueTask<string> Solve_1() => new(_input.SplitNewLine().Select(x =>
     {
         var values = x.Split(" ");
         var player1 = StringToAction(values[0]);
         var player2 = StringToAction(values[1]);
         return new Game(player1, player2);
-    }).Select(x =>
-    {
-        if (x.Player1 == x.Player2)
-        {
-            return (int)x.Player2 + 3;
-        }
-        else if (Player1Wins.Contains(x))
-        {
-            //P1 Win
-            return (int)x.Player2;
-        }
-        else
-        {
-            //P2 win
-            return (int)x.Player2 + 6;
-        }
-    }).Sum().ToString());
+    }).GroupBy(x => x).Select(games => CalculateGameScore(games.Key) * games.Count()).Sum().ToString());
 
     public override ValueTask<string> Solve_2() => new(_input.SplitNewLine().Select(x =>
     {
@@ -47,39 +24,31 @@ public class Day02 : BaseDay
         var gameType = StringToGameType(values[1]);
         var player2 = GetPlayer2Action(player1, gameType);
         return new Game(player1, player2);
-    }).Select(x =>
-    {
-        if (x.Player1 == x.Player2)
-        {
-            return (int)x.Player2 + 3;
-        }
-        else if (Player1Wins.Contains(x))
-        {
-            //P1 Win
-            return (int)x.Player2;
-        }
-        else
-        {
-            //P2 win
-            return (int)x.Player2 + 6;
-        }
-    }).Sum().ToString());
+    }).GroupBy(x => x).Select(games => CalculateGameScore(games.Key) * games.Count()).Sum().ToString());
 
-    private static readonly IDictionary<Action, Action> LoseDictionary = new Dictionary<Action, Action>()
+    private int CalculateGameScore(Game x)
+        => x switch
+        {
+            _ when x.Player1 == x.Player2 => (int)x.Player2 + 3,
+            _ when Player2_LoseDictionary[x.Player1] == x.Player2 => (int)x.Player2,
+            _ => (int)x.Player2 + 6,
+        };
+
+    private static readonly IDictionary<Action, Action> Player2_LoseDictionary = new Dictionary<Action, Action>()
     {
         [Action.Rock] = Action.Scissor,
         [Action.Scissor] = Action.Paper,
         [Action.Paper] = Action.Rock,
     };
 
-    private static readonly  IDictionary<Action, Action> WinDictionary = LoseDictionary.ToDictionary(x => x.Value, y => y.Key);
+    private static readonly IDictionary<Action, Action> Player2_WinDictionary = Player2_LoseDictionary.ToDictionary(x => x.Value, y => y.Key);
 
     private Action GetPlayer2Action(Action player1, GameType gameType)
         => gameType switch
         {
             GameType.Draw => player1,
-            GameType.Lose => LoseDictionary[player1],
-            GameType.Win => WinDictionary[player1],
+            GameType.Lose => Player2_LoseDictionary[player1],
+            GameType.Win => Player2_WinDictionary[player1],
         };
 
 
@@ -113,7 +82,7 @@ public enum GameType
 {
     Draw,
     Win,
-    Lose
+    Lose,
 }
 
 public record Game(Action Player1, Action Player2);
