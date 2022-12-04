9201ab0a84ed4a29b13fda4cd01e6397e56fbf04
Roy <royvou@hotmail.com>
2022-12-04 18:33:03 +0100

    feat: Day 4 - Part 2

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -26,7 +26,19 @@ public class Day04 : BaseDay
     }
 
     public override ValueTask<string> Solve_2()
-        => new("");
+        => new(_input.SplitNewLine().Select(x => x.Split(',').Select(c =>
+        {
+            var b =  c.Split('-').Select(x => x.AsLong()).ToArray();
+            return new Range(b[0], b[1]);
+        }).ToArray()).Count(IsPartialOverLap).ToString());
+    
+    private bool IsPartialOverLap(Range[] lines)
+    {
+        var line1 = lines[0];
+        var line2 = lines[1];
+
+        return line1.End >= line2.Start && line1.Start <= line2.End;
+    }
 }
 
 public record Range(long Start, long End);
