56a23cbe38484d22151b8d86375ddb13c5b9fbef
Roy <royvou@hotmail.com>
2022-12-06 21:19:52 +0100

    feat: Day 6 - Part 1

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -3,7 +3,11 @@
 public class Day06 : Day
 {
     public override ValueTask<string> Solve_1()
-        => throw new NotImplementedException();
+    {
+        var inputArray = _input.ToArray();
+        var result = Enumerable.Range(0, inputArray.Length - 3).First(x => inputArray[x..(x + 4)].GroupBy(x => x).Count() == 4) + 4;
+        return new(result.ToString());
+    }
 
     public override ValueTask<string> Solve_2()
         => throw new NotImplementedException();
